8d2edd78bd2be73635eb06b71efb916977802508
hari5ghb <ghub4868@gmil.com>
2025-04-27 18:14:48 +0530

    scrolling

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import '../Styles/Nav.css'
-import logo from '../assets/logo.png'
+import React, { useState } from 'react';
+import '../Styles/Nav.css';
+import logo from '../assets/logo.png';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Import icons
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-    <div className="nav">
+      <div className="nav">
         <div className="logo">
-            <img  src={logo} alt="" />
+          <img src={logo} alt="Logo" />
         </div>
-        <div>
-            <ul className="nav-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        
-    </div>
-    </>
-  )
-}
 
-export default Nav
+        {/* Menu icon for mobile */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes size={28} /> : <FaBars size={28} />}
+        </div>
+
+        {/* Nav Links */}
+        <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
